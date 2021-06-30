cf5f19b4d1359207ba4db59bb5d199aafb20bff1
arthsan <arthur_sbd@hotmail.com>
2021-06-30 01:30:36 -0300

    fix &apos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <main className={styles.container}>
         <h1>Ragnarok Online API</h1>
         <h2>
-          All the Ragnarok data you'll ever need in one place, easily accessible
+          All the Ragnarok data you will ever need in one place, easily accessible
           through a modern RESTful API.
         </h2>
         <p>Still in develpment!!</p>
