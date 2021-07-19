44411c47a9e9a99b70a9bc8b03cd75faddbd0bd4
arthsan <arthur_sbd@hotmail.com>
2021-07-19 01:58:53 -0300

    add renewal items

---
+++ src/pages/api/v1/re-newal/items/item.tsx
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getItems, mongoConnect } from '../../../../../services/mongodb'
+
+const oldTimes = async (req: NextApiRequest, res: NextApiResponse) => {
+  const {client} = await mongoConnect();
+  const { item } = req.query
+
+  const oldTimes = await getItems(client, 're-newal', item);
+  res.status(200).json(oldTimes[0])
+}
+
+export default oldTimes
